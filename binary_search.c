7e7d6074517ddcef1964f6343cf02d1db1da5fb8
Luara Raggio <luararaggio@hotmail.com>
2024-11-27 15:34:33 -0300

    added iteration to binary_search

--- binary_search.c
+++ binary_search.c
@@ -13,29 +13,29 @@ int binary_search(int *list, int item, int list_len)
     {
         middle = (low + high) / 2;
         guess = list[middle];
-        if (guess == item)
-            return (middle);
+        if (guess == item) return (middle);
         if (guess > item)
             high = middle - 1;
         else
             low = middle + 1;
     }
-    return (-1); //not found
+    return (-1);  // not found
 }
 
 int main(void)
 {
     int list_len;
     int my_list[] = {1, 3, 5, 7, 9};
+    int item_to_find;
+    int result;
 
     list_len = sizeof(my_list) / sizeof(my_list[0]);
-    if (binary_search(my_list, 3, list_len) != -1)
-        printf("my_list[%d]\n", binary_search(my_list, 3, list_len));
+    printf("Enter the number you want to search for: ");
+    scanf("%d", &item_to_find);
+    result = binary_search(my_list, item_to_find, list_len);
+    if (result != -1)
+        printf("Number found at my_list[%d]\n", result);
     else
-        printf("Not found in my_list\n", binary_search(my_list, -1, list_len));
-    if (binary_search(my_list, -1, list_len) != -1)
-        printf("my_list[%d]\n", binary_search(my_list, -1, list_len));
-    else
-        printf("Not found in my_list\n", binary_search(my_list, -1, list_len));
+        printf("Number not found in the list.\n");
     return (0);
 }
